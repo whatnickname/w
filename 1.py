fe0cd62b1f1c584bb2da4ebc1730c17e9a4cefc8
whatnickname <qlqjs1332@gmail.com>
2025-04-17 13:45:08 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -52,4 +52,4 @@ def house_info(index):
    return render_template("house_info.html", location=location, cleaness=cleaness, built_in=built_in, photo=photo)
 
 if __name__ == '__main__':  
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run(debug=True)
